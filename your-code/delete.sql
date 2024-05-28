7eaa61dfde23d47f029fc5d341988cd588904736
TheMineWay <joelcamposoliva9000@gmail.com>
2024-05-28 20:42:30 +0200

    update & delete

---
+++ your-code/delete.sql
@@ -0,0 +1,1 @@
+DELETE cars WHERE id = 4;
---
+++ your-code/update.sql
@@ -0,0 +1,5 @@
+UPDATE sellers s SET s.shop = 'Miami' WHERE s.name = 'Paige Turner';
+
+UPDATE customers c SET c.email = 'ppicasso@gmail.com' WHERE c.name = 'Pablo Picasso';
+UPDATE customers c SET c.email = 'lincoln@us.gov' WHERE c.name = 'Abraham Lincoln';
+UPDATE customers c SET c.email = 'hello@napoleon.me' WHERE c.name = 'Napoléon Bonaparte';
