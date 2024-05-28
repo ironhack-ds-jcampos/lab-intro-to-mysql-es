eea2eeae661d9ad73a970f10938727c8c6d38d8d
TheMineWay <joelcamposoliva9000@gmail.com>
2024-05-28 20:50:58 +0200

    Update delete

--- your-code/delete.sql
+++ your-code/delete.sql
@@ -1,4 +1,1 @@
-/*DELETE FROM cars WHERE id = 4;*/
-
-/* Como he copiado el schema tal cual no tengo campo ID así que borro la priemra fila coincidente. Espero que sirva */
-DELETE FROM cars WHERE vin = 'DAM41UDN3CHU2WVF6' LIMIT 1;
+DELETE FROM cars WHERE id = 4;
